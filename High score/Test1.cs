35c934296fe05a0006fc6638b53e14a38d7ea6ac
ILannanI <Sl0989031@otc.edu>
2025-02-04 22:51:24 -0600

    Nice

---
+++ High score/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace High_score
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ MSTestSettings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Player
+{
+    private string initials;
+    private int score;
+
+    public string Initials
+    {
+        get { return initials; }
+        set { initials = value; }
+    }
+
+    public int Score
+    {
+        get { return score; }
+        set { score = value; }
+    }
+
+    public Player(string initials, int score)
+    {
+        this.initials = initials;
+        this.score = score;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Player player1 = new Player("AB", 100);
+        Console.WriteLine($"Player Initials: {player1.Initials}");
+        Console.WriteLine($"Player Score: {player1.Score}");
+    }
+}
+
